41c90954b3b23bdcb5a2440fe4c46f40518ee9ba
Agustin Aliaga <agustinaliagac@gmail.com>
2015-06-06 14:37:19 -0300

    fixed manifest issue

--- EpiAPP/app/src/main/java/com/epi/epiapp/Encuesta/EncuestaParteInicial.java
+++ EpiAPP/app/src/main/java/com/epi/epiapp/Encuesta/EncuestaParteInicial.java
@@ -1,18 +1,34 @@
 package com.epi.epiapp.Encuesta;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.epi.epiapp.R;
 
 public class EncuestaParteInicial extends ActionBarActivity {
 
+    EditText nombreET;
+    EditText edadET;
+    EditText calleET;
+    EditText numeroET;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encuesta_parte_inicial);
+
+         nombreET = (EditText) findViewById(R.id.nombreET);
+         edadET = (EditText) findViewById(R.id.edadET);
+         calleET = (EditText) findViewById(R.id.calleET);
+         numeroET = (EditText) findViewById(R.id.numeroET);
+
+
     }
 
     @Override
@@ -31,9 +47,31 @@ public class EncuestaParteInicial extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_ok) {
-            return true;
+            validateItems();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void validateItems() {
+
+        String nombre = nombreET.getText().toString();
+        String edad = edadET.getText().toString();
+        String calle = calleET.getText().toString();
+        String num = numeroET.getText().toString();
+
+        if (!nombre.isEmpty() && !edad.isEmpty() && !calle.isEmpty() && !num.isEmpty() ){
+            nextStep();
+        } else {
+            Toast.makeText(EncuestaParteInicial.this,"Debe completar todos los campos",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void nextStep() {
+
+        Intent i = new Intent(EncuestaParteInicial.this,EncuestaVivienda.class);
+        startActivity(i);
+
     }
 }
